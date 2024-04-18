4e940b58d2fede1328035e0670e1c759de1d051d
Arden-Feldt <feldt@unc.edu>
2024-04-18 08:31:11 -0400

    Testing bugs, the bugs remain

--- Testing.py
+++ Testing.py
@@ -1,4 +1,4 @@
 import GSD_name_to_id
 
 if __name__ == '__main__':
-    GSD_name_to_id.gsd_render("GSDs/modifiable_UNC_gsd.gsd", "GSDs/clone_of_modifile.gsd", 20, "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode/pythonProject1/ImageReader/Images/white.jpeg")
+    GSD_name_to_id.gsd_render("GSDs/modifiable_UNC_gsd.gsd", "GSDs/clone_of_modifile.gsd", 35, "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode/pythonProject1/ImageReader/Images/UNCLogo.jpeg")
