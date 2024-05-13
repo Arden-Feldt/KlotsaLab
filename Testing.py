fcf43a729d42c4d3ced8c8bc860385d2eed425d2
Arden-Feldt <feldt@unc.edu>
2024-05-13 14:06:52 -0400

    fixed formatting in main

--- Testing.py
+++ Testing.py
@@ -3,4 +3,8 @@ import renderer
 
 # Where you pass in all relevant information to create the visual
 if __name__ == '__main__':
-    renderer.gsd_render("GSDs/modifiable_UNC_gsd.gsd", "GSDs/clone_of_modifile.gsd", 12, "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode/pythonProject1/ImageReader/Images/UNCLogo.jpeg")
+    renderer.gsd_render("GSDs/modifiable_UNC_gsd.gsd",
+                        "GSDs/clone_of_modifile.gsd",
+                        12,
+                        "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode/pythonProject1/ImageReader/Images/"
+                        "UNCLogo.jpeg")
