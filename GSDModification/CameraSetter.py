812d33767930f1b186e613ec2026fb04d39b3d9b
Arden-Feldt <feldt@unc.edu>
2024-05-16 15:34:04 -0400

    camera setter object

---
+++ GSDModification/CameraSetter.py
@@ -0,0 +1,44 @@
+import gsd.hoomd
+
+from Testing import box_dim
+
+
+class CameraSetter:
+
+    def __init__(self, input_file, output_file):
+        self.input_file = input_file
+        self.output_file = output_file
+
+    def set_cam(self):
+        """Sets the camera up for the specific simulations, so its centered"""
+        print("started setting")
+
+        half_step = box_dim / 2
+
+        # Read in the file with the intent to name
+        with gsd.hoomd.open(name=self.input_file, mode="r") as file:
+            with gsd.hoomd.open(name=self.output_file, mode="w") as modified_file:
+                for frame in file:
+                    # Get the positions of all particles in the current frame
+                    particle_positions = frame.particles.position
+
+                    # Iterate over each particle position
+                    for particle_pos in particle_positions:
+                        # Update the x and y-coordinates of the particles
+                        if particle_pos[0] < 0:
+                            particle_pos[0] = (particle_pos[0] + half_step)
+                        else:
+                            particle_pos[0] = (particle_pos[0] - half_step)
+
+                        if particle_pos[1] <= half_step:
+                            particle_pos[1] = (particle_pos[1] + (half_step / 2))
+                        else:
+                            particle_pos[1] = (particle_pos[1] - (3 / 2 * half_step))
+
+                        if particle_pos[1] >= half_step:
+                            particle_pos[1] = particle_pos[1] - (2 * half_step)
+
+                    # Update the positions of particles in the frame
+                    frame.particles.position[:] = particle_positions
+
+                    modified_file.append(frame)
--- Testing.py
+++ Testing.py
@@ -1,9 +1,14 @@
 import Renderer
+from GSDModification import CameraSetter
 
 box_dim = 361.8006286621094
 
 # Where you pass in all relevant information to create the visual
 if __name__ == '__main__':
+
+    # Broken Camera Set
+    # camset = CameraSetter.CameraSetter("GSDs/modifiable_UNC_gsd.gsd", "GSDs/centered_GSD.gsd")
+
     renderer = Renderer.Renderer("GSDs/modifiable_UNC_gsd.gsd",
                                  "GSDs/clone_of_modifile.gsd",
                                  100,
