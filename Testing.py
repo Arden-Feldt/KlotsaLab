28e14c4e6150668561db17ed3ea2a3b93d88323f
Arden-Feldt <feldt@unc.edu>
2024-05-17 15:13:16 -0400

    added todos

--- Testing.py
+++ Testing.py
@@ -5,14 +5,16 @@ box_dim = 361.8006286621094
 
 # Where you pass in all relevant information to create the visual
 if __name__ == '__main__':
-
     # Broken Camera Setter
+    # TODO: fix camera setter
     # camset = CameraSetter.CameraSetter("GSDs/modifiable_UNC_gsd.gsd", "GSDs/centered_GSD.gsd")
 
     # num_bins must be a factor of image height and size
+    # TODO: Optimize for loops
+    # TODO: Make colorlist -> bins a hashset
     renderer = Renderer.Renderer("GSDs/modifiable_UNC_gsd.gsd",
                                  "GSDs/clone_of_modifile.gsd",
-                                 200,
+                                 600,
                                  "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode"
                                  "/pythonProject1/ImageReader/Images/UNCLogo.jpeg")
 
