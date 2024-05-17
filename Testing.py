c1ca4bd0ff37440cd94daa36430bd3dc4ed281cc
Arden-Feldt <feldt@ad.unc.edu>
2024-05-17 17:08:10 -0400

    fixed camera

--- Testing.py
+++ Testing.py
@@ -6,16 +6,17 @@ box_dim = 361.8006286621094
 # Where you pass in all relevant information to create the visual
 if __name__ == '__main__':
     # Broken Camera Setter
-    # TODO: fix camera setter
     # camset = CameraSetter.CameraSetter("GSDs/modifiable_UNC_gsd.gsd", "GSDs/centered_GSD.gsd")
+    # camset.set_cam()
 
     # num_bins must be a factor of image height and size
     # TODO: Optimize for loops
     # TODO: Make colorlist -> bins a hashset
-    renderer = Renderer.Renderer("GSDs/modifiable_UNC_gsd.gsd",
+    renderer = Renderer.Renderer("GSDs/centered_GSD.gsd",
                                  "GSDs/clone_of_modifile.gsd",
                                  100,
                                  "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode"
                                  "/pythonProject1/ImageReader/Images/UNCLogo.jpeg")
 
+    # TODO: Add color
     renderer.gsd_render()
