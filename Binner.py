6118ee3f9962b375c84b3d942a2c0303d18987cc
Arden-Feldt <feldt@ad.unc.edu>
2024-05-20 14:34:03 -0400

    removed unoptimized binning method

--- Binner.py
+++ Binner.py
@@ -11,6 +11,8 @@ class Binner:
         self.highlight_bin_list = highlight_bin_list
 
     def optimize_binning(self):
+        """Used to create complex images in simulation by cutting it into manageable bins - assigns names"""
+
         print("started opt-binning")
 
         particle_names = {}
@@ -32,33 +34,6 @@ class Binner:
             particle_names[particle_idx] = particle_name
 
         print("finished opt-binning")
-        return particle_names
-
-    def binning_method(self):
-        # TODO: OPTIMIZE this
-        print("started binning")
-
-        particle_names = {}
-
-        """Used to create comple images in simulation by cutting it into managable bins - assigns names"""
-        for particle_idx in range(self.frame.particles.N):
-            particle_position = self.frame.particles.position[particle_idx]
-
-            current_bin = 0
-
-            for x in np.arange(-box_dim / 2, box_dim / 2, box_dim / self.num_bins):
-                for y in np.arange(-box_dim / 2, box_dim / 2, box_dim / self.num_bins):
-                    # check to see if particle is in current bin
-                    if x <= particle_position[0] < x + box_dim / self.num_bins and y <= \
-                            particle_position[1] < y + box_dim / self.num_bins:
-                        # name particle given on if bin is in binlist
-                        particle_name = 1 if current_bin in self.highlight_bin_list else 0
-                        # Store the particle name in the dictionary
-                        particle_names[particle_idx] = particle_name
-                    current_bin += 1
-                    y += box_dim / self.num_bins
-                x += box_dim / self.num_bins
-        print("finished binning")
         return particle_names
 
     '''
