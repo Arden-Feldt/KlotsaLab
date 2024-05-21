d046df0c95deac736f3c578307aa55bfc55e4a95
Arden-Feldt <feldt@ad.unc.edu>
2024-05-21 14:19:03 -0400

    tuned to my satisfaction

--- Testing.py
+++ Testing.py
@@ -1,5 +1,4 @@
 import Renderer
-from GSDModification import ModifierManager
 from GSDModification.ModifierManager import set_camera, copy_gsd, check_write
 
 box_dim = 361.8006286621094
@@ -8,22 +7,25 @@ box_dim = 361.8006286621094
 if __name__ == '__main__':
 
     # function used to copy a gsd to a new file
-    copy_gsd("modifiable_UNC_gsd", "centered_gsd")
+    # copy_gsd("modifiable_UNC_gsd.gsd", "centered_gsd.gsd")
 
     # TODO: make this more general
     # sets the camera to a specific location, only useful if you have my gsd.
-    set_camera("centered_gsd")
+    # set_camera("modifiable_UNC_gsd.gsd", "centered_gsd.gsd")
 
     # check if you can write to a specific gsd
-    check_write("clone_of_modifile")
+    # check_write("clone_of_modifile.gsd")
 
     # num_bins must be a factor of image height and size
     # TODO: Make colorlist -> bins a hashset
     renderer = Renderer.Renderer("centered_gsd.gsd",
                                  "clone_of_modifile.gsd",
-                                 100,
+                                 600,
                                  "/Users/ethan/feldt_dKlotsa_work/KlotsaPythonCode"
-                                 "/pythonProject1/ImageReader/Images/UNCLogo.jpeg")
+                                 "/pythonProject1/ImageReader/Images/clusterUNC.jpeg")
 
-    # TODO: Add color
     renderer.gsd_render()
+
+    # TODO: Add post proc
+    # TODO: Add color
+
