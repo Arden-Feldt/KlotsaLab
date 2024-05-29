d87e4776894cada369a1d67bf88830aa8c466d0a
Arden-Feldt <feldt@ad.unc.edu>
2024-05-29 14:06:13 -0400

    camera shift logic works now, but only produces one frame

--- GSDModification/CameraSetter.py
+++ GSDModification/CameraSetter.py
@@ -1,45 +1,47 @@
 import gsd.hoomd
 
-
 class CameraSetter:
 
     def __init__(self, input_file, output_file):
-        self.input_file = "GSDs/" + input_file
-        self.output_file = "GSDs/" + output_file
+        self.input_file = input_file
+        self.output_file = output_file
 
-    def set_cam(self):
-        # TODO: make this more general
+    def set_cam(self, x_dim, y_dim, right_shift, up_shift):
         """Sets the camera up for the specific simulations, so its centered"""
         print("started setting")
 
-        half_step = 361.8006286621094 / 2
-
-        # Read in the file with the intent to name
-        with gsd.hoomd.open(name=self.input_file, mode="r") as file:
-            with gsd.hoomd.open(name=self.output_file, mode="w") as modified_file:
-                for frame in file:
+        # Open the input file for reading
+        with gsd.hoomd.open(name=self.input_file, mode="r") as input_file:
+            # Open the output file for writing
+            with gsd.hoomd.open(name=self.output_file, mode="w") as output_file:
+                for frame in input_file:
                     # Get the positions of all particles in the current frame
                     particle_positions = frame.particles.position
 
                     # Iterate over each particle position
-                    for particle_pos in particle_positions:
+                    for i, particle_pos in enumerate(particle_positions):
                         # Update the x and y-coordinates of the particles
-                        if particle_pos[0] < 0:
-                            particle_pos[0] = (particle_pos[0] + half_step)
-                        else:
-                            particle_pos[0] = (particle_pos[0] - half_step)
+                        particle_pos[0] = (particle_pos[0] + right_shift) % x_dim
+                        particle_pos[1] = (particle_pos[1] + up_shift) % y_dim
 
-                        if particle_pos[1] <= half_step:
-                            particle_pos[1] = (particle_pos[1] + (half_step / 2))
-                        else:
-                            particle_pos[1] = (particle_pos[1] - (3 / 2 * half_step))
+                        # Ensure the particles are within the bounds
+                        if particle_pos[0] < -x_dim / 2:
+                            particle_pos[0] += x_dim
+                        elif particle_pos[0] > x_dim / 2:
+                            particle_pos[0] -= x_dim
 
-                        if particle_pos[1] >= half_step:
-                            particle_pos[1] = particle_pos[1] - (2 * half_step)
+                        if particle_pos[1] < -y_dim / 2:
+                            particle_pos[1] += y_dim
+                        elif particle_pos[1] > y_dim / 2:
+                            particle_pos[1] -= y_dim
 
-                    # Update the positions of particles in the frame
-                    frame.particles.position[:] = particle_positions
+                        # Update the positions of particles in the frame
+                        frame.particles.position[i] = particle_pos
 
-                    modified_file.append(frame)
+                    # Append the modified frame to the output file
+                    output_file.append(frame)
 
         print("finished setting")
+
+
+
