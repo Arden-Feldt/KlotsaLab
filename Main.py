1b7f7909c582dc62fa1768d62c6d3f550dba1cc5
Arden-Feldt <feldt@ad.unc.edu>
2024-05-29 15:06:55 -0400

    updated comments

--- Main.py
+++ Main.py
@@ -10,13 +10,12 @@ if __name__ == '__main__':
     # set_camera(<input gsd>, <output gsd>, <right shift>, <up_shift>)
     # check_write(<gsd with unclear permissions>)
 
-    input_gsd = "GSDs/centered_gsd.gsd"                              # the gsd you'll read in and copy
-    output_gsd = "GSDs/clone_of_modifile.gsd"                        # the gsd the program will make, update, and save
-    num_bins = 600                                                   # the # of bins on an axis, total bins = num_bins^2
-    image_path = "ImageReader/Images/clusterUNC.jpeg"                # Path to black and white image you're using
-    image_frame = get_final_frame(input_gsd) - 1            # Frame where the image comes together
+    input_gsd = "GSDs/centered_gsd.gsd"                             # the gsd you'll read in and copy
+    output_gsd = "GSDs/clone_of_modifile.gsd"                       # the gsd the program will make, update, and save
+    num_bins = 600                                                  # the # of bins on an axis, total bins = num_bins^2
+    image_path = "ImageReader/Images/clusterUNC.jpeg"               # Path to black and white image you're using
+    image_frame = get_final_frame(input_gsd) - 1                    # Frame where the image comes together
 
-    # num_bins must be a factor of image height and size
     renderer = Renderer.Renderer(input_gsd,
                                  output_gsd,
                                  num_bins,
